0f8f2070d48cae62d5c8ce5cf1a678af0cd2d5e1
Ruben Olano <rubendavid2711@gmail.com>
2022-02-04 16:37:25 -0600

    add getting trends from twitter

--- Typings/structures/Client.d.ts
+++ Typings/structures/Client.d.ts
@@ -2,6 +2,7 @@ import { Client as DiscordClient, ClientOptions } from "discord.js";
 declare class Client extends DiscordClient {
     constructor(options: ClientOptions, token: string);
     start(): void;
+    fetchData(): Promise<any>;
 }
 export { Client };
 //# sourceMappingURL=Client.d.ts.map
--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,4 +1,14 @@
-import { Client as DiscordClient, ClientOptions, Message } from "discord.js";
+import axios from "axios";
+import {
+  Client as DiscordClient,
+  ClientOptions,
+  Message,
+  MessageEmbed,
+} from "discord.js";
+import { config } from "dotenv";
+config();
+const twit_token = process.env.TWIT_TOKEN;
+
 class Client extends DiscordClient {
   public constructor(options: ClientOptions, token: string) {
     super(options);
@@ -16,7 +26,42 @@ class Client extends DiscordClient {
 
     this.on("messageCreate", async (message: Message) => {
       if (message.author.bot) return;
-      if (message.content === "test") await message.reply("test");
+      if (message.cleanContent === "test") await message.reply("test");
+      else if (message.cleanContent === "profile")
+        await message.channel.send({
+          embeds: [
+            new MessageEmbed()
+              .setTitle("Profile")
+              .setThumbnail(message.author.avatarURL())
+              .addField("Name", message.author.tag, true),
+          ],
+        });
+      else if (message.cleanContent === "trending") {
+        const trends = await this.fetchData();
+        await message.channel.send({
+          embeds: trends,
+        });
+      }
+    });
+  }
+
+  async fetchData() {
+    const { data } = await axios.get(
+      "https://api.twitter.com/1.1/trends/place.json?id=23424977",
+      {
+        headers: {
+          Authorization: `Bearer ${twit_token}`,
+        },
+      }
+    );
+
+    return data[0].trends.slice(0, 10).map((item) => {
+      return new MessageEmbed()
+        .setTitle(item.name)
+        .addField("# of tweets", `${item.tweet_volume}`, true)
+        .setThumbnail(item.url)
+        .setColor("BLUE")
+        .setURL(item.url);
     });
   }
 }
