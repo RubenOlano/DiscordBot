19bae17e04972c7fb430497c3d6f0de3629582ac
Ruben Olano <rubendavid2711@gmail.com>
2022-02-03 19:00:33 -0600

    setup discord client

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,11 @@
-console.log("hello world");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const discord_js_1 = require("discord.js");
+const { TOKEN } = process.env;
+const client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS] });
+client.once('ready', () => {
+    console.log('ready');
+});
+client.login(TOKEN);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world")
+import 'dotenv/config'
+import { Client, Intents } from 'discord.js'
+const { TOKEN } = process.env
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS]})
+
+client.once('ready', () => {
+    console.log('ready')
+})
+
+client.login(TOKEN)
