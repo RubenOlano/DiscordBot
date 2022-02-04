17ec1acf6802dcb3466cd4ac9b106fb9eb39861e
Ruben Olano <rubendavid2711@gmail.com>
2022-02-04 10:00:04 -0600

    setup basic logic

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const discord_js_1 = require("discord.js");
 const { TOKEN } = process.env;
-const client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS] });
+const client = new discord_js_1.Client({ intents: ['GUILDS', 'GUILD_VOICE_STATES', 'GUILD_MEMBERS', 'GUILD_MESSAGES'] });
 client.once('ready', () => {
-    console.log('ready');
+    var _a;
+    console.log(`Logged in as ${(_a = client.user) === null || _a === void 0 ? void 0 : _a.tag}`);
+});
+client.on('messageCreate', async (message) => {
+    if (message.author.bot)
+        return;
+    console.log(message);
+    await message.channel.send('hi');
 });
 client.login(TOKEN);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import 'dotenv/config'
-import { Client, Intents } from 'discord.js'
+import { Client } from 'discord.js'
 const { TOKEN } = process.env
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS]})
+const client = new Client({intents: ['GUILDS', 'GUILD_VOICE_STATES', 'GUILD_MEMBERS', 'GUILD_MESSAGES']})
 
 client.once('ready', () => {
-    console.log('ready')
+    console.log(`Logged in as ${client.user?.tag}`)
+})
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot)
+        return;
+    console.log(message)
+    await message.channel.send('hi')
 })
 
 client.login(TOKEN)
